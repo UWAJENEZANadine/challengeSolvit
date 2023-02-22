768a1202e932f2853ddc9f18b2f3fc79d9a8c2c5
UWAJENEZANadine <adelineu640@gmail.com>
2023-02-22 10:53:34 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+let name = 'Nadine'
+let age = 21
+let gender = "Female"
+age = 15;
+console.log('my name is ' + name + ' and I am aged ' + age);
+
+const arr =[1,4,5,2,'hello',8];
+arr.push("values")
+
+for(let i = 0; i<arr.length; i++){
+
+console.log(arr[i])
+
+}
+
+let text1 = "Hello";
+let text2 = "world!";
+let result = text1.concat(" ", text2);
+console.log(result);
